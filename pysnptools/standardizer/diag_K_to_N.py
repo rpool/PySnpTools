68b1284c7d64e817acc3876a14e0f39bd5ee8e5a
Chris Widmer <chwidmer@microsoft.com>
2015-02-03 12:42:03 -0800

    stopped dealing with writeable flag in DiagKtoN

--- pysnptools/standardizer/diag_K_to_N.py
+++ pysnptools/standardizer/diag_K_to_N.py
@@ -6,19 +6,16 @@ class DiagKtoN(object):
         self._N = N
 
     def standardize(self, snps, blocksize=None, force_python_only=False):
-        flag = snps.flags.writeable
+
         vec = snps.reshape(-1, order="A")
         
         # make sure no copy was made
         assert vec.base is snps
         squared_sum = vec.dot(vec)
         factor = 1./np.sqrt(squared_sum / float(self._N))
-        
-        snps.flags.writeable = flag
-        
         snps *= factor
-        
+                
         return snps
 
     def __repr__(self): 
-            return "{0}()".format(self.__class__.__name__)
+        return "{0}()".format(self.__class__.__name__)
